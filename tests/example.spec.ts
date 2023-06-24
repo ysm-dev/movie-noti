e154ca2761520634dc016c6815078813ce6c7ede
Chris <i@ysm.dev>
2023-06-24 23:17:44 +0900

    test

---
+++ tests/example.spec.ts
@@ -0,0 +1,54 @@
+import { test, expect } from '@playwright/test'
+import { readFile, readdir } from 'fs/promises'
+import { TOKENS, getRandom } from './getRandom'
+import { pipe, map, toArray, tap, toAsync, concurrent } from '@fxts/core'
+import { sendDiscordMessage } from './sendDiscordMessage'
+
+test('has title', async ({ context, page }) => {
+  const page1 = await context.newPage()
+  const page2 = await context.newPage()
+
+  await Promise.all([
+    page1.goto(`https://search.naver.com/search.naver?query=현재상영영화`),
+    page2.goto(`https://search.naver.com/search.naver?query=개봉예정영화`),
+  ])
+
+  await Promise.all([
+    page1
+      .locator('._au_movie_list_content_wrap')
+      .screenshot({ path: `./temp/current.png` }),
+    page2
+      .locator('._au_movie_list_content_wrap')
+      .screenshot({ path: `./temp/future.png` }),
+  ])
+
+  await page1.close()
+  await page2.close()
+
+  const urls = await pipe(
+    [`./temp/current.png`, `./temp/future.png`],
+    toAsync,
+    map(readFile),
+    map((body) =>
+      fetch('https://api.nft.storage/upload/', {
+        headers: {
+          authorization: `Bearer ${getRandom(TOKENS)}`,
+          'content-type': 'image/png',
+        },
+        method: 'POST',
+        body,
+      }).then((res) => res.json()),
+    ),
+
+    concurrent(2),
+    map((data) => `https://${data.value.cid}.ipfs.cf-ipfs.com`),
+    toArray,
+  )
+
+  await sendDiscordMessage('#movie', urls[0])
+  await sendDiscordMessage('#movie', urls[1])
+
+  console.log('DONE!!!')
+
+  expect(1).toBe(1)
+})
---
+++ tests/sendDiscordMessage.ts
@@ -0,0 +1,21 @@
+export const sendDiscordMessage = (
+  channel: `#${keyof typeof channelMap}`,
+  message: string | object,
+) => {
+  const key = channelMap[channel.slice(1) as keyof typeof channelMap]
+  return fetch(`https://discord.com/api/webhooks/${key}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(
+      typeof message === 'string' ? { content: message } : message,
+    ),
+  }).catch(() => {
+    console.error('Failed to send Discord message', message)
+  })
+}
+
+const channelMap = {
+  movie: `1122167462285291680/2BBXTJRzcLM07f6uYTo3udTKPB4PEYQsSDH3Zio1xdnTyRZ-3t4sOtbHc7Q6Ds3duKgl`,
+} as const

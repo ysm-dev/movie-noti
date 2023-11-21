aee52d840c31c3088d32da5dcb4bf3327264df9c
ysm-dev <i@ysm.dev>
2023-11-21 19:13:58 +0900

    headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless: false,
+    headless: true,
     trace: "on-first-retry",
   },
 

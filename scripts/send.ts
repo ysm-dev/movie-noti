a7daac05db71a0156a49ff607eff458f7e7eb04f
ysm-dev <i@ysm.dev>
2024-07-13 21:48:18 +0900

    test

--- scripts/send.ts
+++ scripts/send.ts
@@ -1,13 +1,15 @@
 import { sendDiscordMessage } from '../tests/sendDiscordMessage'
 
 async function main() {
+  const randomString = Math.random().toString(36).substring(7)
+
   await sendDiscordMessage(
     '#movie',
-    `https://github.com/ysm-dev/movie-noti/raw/main/temp/current.png`,
+    `https://github.com/ysm-dev/movie-noti/raw/main/temp/current.png?${randomString}`,
   )
   await sendDiscordMessage(
     '#movie',
-    `https://github.com/ysm-dev/movie-noti/raw/main/temp/future.png`,
+    `https://github.com/ysm-dev/movie-noti/raw/main/temp/future.png?${randomString}`,
   )
 }
 
